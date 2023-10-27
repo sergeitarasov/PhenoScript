48a16675c933d4b949f9072b3d45137818b802e3
Sergei Tarasov <sergxf@yandex.ru>
2023-10-27 18:12:32 +0300

    v 20

--- phenospy_package/setup.py
+++ phenospy_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phenospy',
-    version='0.210',
+    version='0.220',
     packages=find_packages(exclude=['tests*', 'devel*', 'build*']),
     license='MIT',
     description='Tools for making and processing computable phenotypes',
