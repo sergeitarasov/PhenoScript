d78661e8cb2d44a0b646f470fae65c318e02c5f0
Sergei Tarasov <sergei.tarasov@helsinki.fi>
2026-07-28 15:28:35 +0300

    new species yaml block fixed

--- phenospy_package/setup.py
+++ phenospy_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phenospy',
-    version='0.277',
+    version='0.278',
     packages=find_packages(exclude=['tests*', 'devel*', 'build*']),
     license='MIT',
     description='Tools for making and processing computable phenotypes',
